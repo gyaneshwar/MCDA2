574101938fece954903e46d240ae0db23c459cba
AllenMax <allenm55@hotmail.com>
2019-06-21 14:02:08 -0300

    update

--- Data mining/Assignment3/allen_association.r
+++ Data mining/Assignment3/allen_association.r
@@ -16,15 +16,39 @@ write.table(df_user,"Milestones2.csv", quote=FALSE, row.names = FALSE, col.names
 tr = read.transactions("Milestones2.csv",format="basket",sep=",")
 summary(tr)
 itemFrequencyPlot(tr, topN=10)
-rules = apriori(tr,parameter = list(supp=0.009,conf=0.5))
+
+#-------------------------------------------------------------
+#supp = 0.03
+rules = apriori(tr,parameter = list(supp=0.03,conf=0.5))
 inspect(rules)
-rules = apriori(tr,parameter = list(supp=0.3,conf=0.5))
-inspect(rules)
-rules = apriori(tr,parameter = list(supp=0.2,conf=0.5))
-inspect(rules)
+#supp = 0.03 (Gives No Rules)
+#
+#-------------------------------------------------------------
+#supp = 0.02
 rules = apriori(tr,parameter = list(supp=0.02,conf=0.5))
 inspect(rules)
+#supp = 0.02 (Gives 17 Rules)
+#-------------------------------------------------------------
+#supp = 0.015
+rules = apriori(tr,parameter = list(supp=0.015,conf=0.5))
+inspect(rules)
+#-------------------------------------------------------------
+#supp = 0.01
+rules = apriori(tr,parameter = list(supp=0.01,conf=0.5))
+inspect(rules)
+#-------------------------------------------------------------
+#supp = 0.009
+rules = apriori(tr,parameter = list(supp=0.005,conf=0.5))
+inspect(rules)
+
 inspect(sort(rules,by='lift')[1:15])
 itemsets=unique(generatingItemsets(rules))
 itemsets
 inspect(itemsets)
+
+
+#
+help(apriori)
+maxrules = apriori(tr,list(supp=0.02,conf=0.5, target="maximally frequent itemsets"))
+inspect(sort(maxrules))
+
