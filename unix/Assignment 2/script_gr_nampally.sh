9541f555c8fe7ea73c54c4b6bc669fdfc058e898
gyaneshwar <rao.gyaneshwar@gmail.com>
2019-07-06 02:08:39 -0300

    updated script_gr_nampally.sh
    loved the try catch implementation in a language which it does have the feature.

--- unix/Assignment 2/script_gr_nampally.sh
+++ unix/Assignment 2/script_gr_nampally.sh
@@ -1,145 +1,197 @@
+
+#########################################################
+#########################################################
+#														#
+# TEAM													#
+# =====													#
+#														#
+# Gyaneshwar Rao Nampally A00433014						#
+# Sachit Jain A00432721									#
+# Allen A00432526										#
+#														#
+# Execution command : . script_gr_nampally.sh 			#
+#														#
+#														#
+# Output:												#
+#														#
+# out/app.py 											#
+# out/serv.html 										#
+#														#
+#														#
+# note: The files app.py and serv.html are saved 		#
+# exclusively as the output of the program. 			#
+#														#
+#########################################################
+#########################################################
+
 #!/bin/bash
 
-echo -e "====================================================="
-echo -e "Running Script File"
-echo -e "=====================================================\n"
+echo  "====================================================="
+echo  "Running Script File"
+echo  "=====================================================\n"
 
-#Quesion 1
-if [ -d out ]
-then
-        echo -e "Folder 'out' exist. Removing now"
-        rm -rf out
-        echo -e "Existing Folder 'out' Deleted Successfully \n"
-fi
+try ()
+{
+    [[ $- = *e* ]]; SAVED_OPT_E=$? #saving the exit state to SAVED_OPT_E
+    set +e #setting the environment variable and allow the program to continue irrespective of the error.
+}
 
+throw ()
+{
+    exit $1 #capturing the first parameter to the function and accessing the parameter using $1
+}
 
-echo -e "Creating Folder 'out'"
-mkdir ./out
-echo -e "Folder Created Successfully \n"
+catch ()
+{
+    export ex_code=$? #capturing the exit code produced in try block.
+    (( $SAVED_OPT_E )) && set +e #allowing the program to proceed irrespective of the error code presented.
+    return $ex_code # returning the program with the presented exit code.
+}
 
-#Question 2
-echo -e "Entering Folder 'out'"
-cd out
-echo -e "Inside  Folder 'out'"
-
-#Question 3
-echo -e "Downloading the 'Dockerfile' and 'app.py' file from 'http://lnx.cs.smu.ca/docker/'"
-wget lnx.cs.smu.ca/docker/Dockerfile
-wget lnx.cs.smu.ca/docker/app.py
-
-if [ -f Dockerfile ] && [ -f app.py ]
-then
-	echo -e "Files were Downloaded Successfully, as seen below: \n"
-else
-	echo -e "Files not found\n"
-fi
-
-#Question 4
-echo -e "Checking the date and replacing the content in app.py"
-
-check_DD=$(($(date +%e)%2))
-
-if [ $check_DD -eq 1 ]
-then
-	sed -i 's/Hello World!/Today is an odd day/g' app.py
-else
-	sed -i 's/Hello World!/Today is an even day/g' app.py
-fi
-
-#Question 5
-docker build -t gr_nampally_a2 .
-#Question 6
-echo -e "Checking for available ports"
-port=$(nc -zv lnx.cs.smu.ca 1999-2500 2>&1 | grep -m 1 refused | awk '{ print $6 }')
-echo -e port
-echo -e "Running docker on "$port
-
-docker run -p $port:80 gr_nampally_a2 &
-#Question 7
-docker_id = $(docker container ps -a | grep gr_nampally_a1 | awk '{ print $1 }')
-docker inspect --format '{{ .NetworkSettings.IPAddress }}' $docker_id
-
-#Question 8
-test_http = $(curl -s -o /dev/null -w "%{http_code}" lnx.cs.smu.ca:$port)
-
-if [ test_http -eq 200 ]
-then
-echo -e "Website is working"
-else
-echo -e "Website cant be reached, terminating the program"
-destroy_function
-fi
-
-#Question 9
-wget lnx.cs.smu.ca:$port -O serv.html
-
-if [ -f serv.html ]
-then 
-echo -e "serv.html downloaded"
-else
-echo -e "serv.html download failed, terminating the program"
-destroy_function
-fi
-
-#Question 11
-if [ $? -eq 0 ]
-then
-echo "Success"
-destroy_function
-else
-echo "error has occured"
-destroy_function
-if
-
+#garbage collecting function
 destroy_function () {
-#destroying the process id's
-ps | grep docker | awk '{ print $1 }' > process_ids
+	#destroying the process id's which are running docker.
+	ps | grep docker | awk '{ print $1 }' > process_ids # capturing the ID's of the docker processes.
 
-#looping and killing each process_ids
-while read p; do
-echo "$p"
-kill -9 $p
-done < process_ids
+	#looping and killing each process_ids
+	while read p; do
+	echo "$p"
+	kill -9 $p #killing the processes of docker.
+	done < process_ids
 
-#removing the process_ids temp file
-rm process_ids
-
-cd ..
+	#removing the docker containers
+	docker container ps -a | grep gr_nampally | awk '{ print $1 }' > docker_ids #capturing the ID's of docker containers.
+	if [ -f docker_ids ]
+	then
+		while read docker_id; do
+			docker container rm -f $docker_id #removing the docker containers.
+		done < docker_ids
+	fi
+	sleep 3
+	cd ~/script
+	rm -f out/Dockerfile #removing the 'Dockerfile' image.
+	rm -f out/*ids #removing buffers created by the program.
+	rm -f *ids #removing buffers created by the program.
+	echo "Clean-up success"
 }
 
 
+#Quesion 1
+#check if the 'out' folder does exists, if it does, then delete the folder and create afresh.
+program () {
+	
+	try
+	(
+	if [ -d out ] # check the folder 'out'
+	then
+	        echo  "Folder 'out' exist. Removing now"
+	        rm -rf out || throw 1 # -r recursively remove the files inside the directory, -f force the deletion operation irrespective of permissions.
+	        echo  "Existing Folder 'out' Deleted Successfully \n"
+	fi
+	echo  "Creating Folder 'out'"
+	mkdir ./out #creating the folder out.
+	echo  "Folder Created Successfully \n"
+	
+#Question 2
+#Enter into the 'out' folder.
+	echo  "Entering Folder 'out'"
+	cd out
+	echo  "Inside  Folder 'out'"
 
 
+#Question 3
+#Download Dockerfile and app.py and check if they exists in the local directory.
+	echo  "Downloading the 'Dockerfile' and 'app.py' file from 'http://lnx.cs.smu.ca/docker/'"
+	wget lnx.cs.smu.ca/docker/Dockerfile || throw 1
+	wget lnx.cs.smu.ca/docker/app.py || throw 1
 
+	if [ -f Dockerfile ] && [ -f app.py ] #verifying the existance of the files.
+	then
+		echo  "Files were Downloaded Successfully, as seen below: \n"
+	else
+		echo  "Files not found\n"
+		throw 1 #aborting the program if the functions didnt perform properly.
+	fi
 
+#Question 4
+#Check the 'day' and evaluate if it is odd or even 
+#and replace the text 'Hello world' in app.py accordingly.
+	echo  "Checking the date and replacing the content in app.py"
 
+	check_DD=$(($(date +%e)%2)) # capturing the present day and performing mathematical function mod 2.
 
+	if [ $check_DD -eq 1 ] #checking if the function is equal to 1 (which determines if it is even or odd)
+	then
+		sed -i 's/Hello World!/Today is an odd day/g' app.py || throw 1 #replacing Hello world with 'Today is an odd day'
+	else
+		sed -i 's/Hello World!/Today is an even day/g' app.py || throw 1 #replacing Hello world with 'Today is an even day'
+	fi
 
+#Question 5
+	(docker build -t gr_nampally .) || throw 1 #building the docker image with the name 'gr_nampally' 
 
+	port=0 #initializing the port variable.
+#Question 6
+	echo  "Checking for available ports"
+	port=$(nc -zv lnx.cs.smu.ca 1999-2500 2>&1 | grep -m 1 refused | awk '{ print $6 }') #checking for the free ports in the range of 1999, 2500
+	echo  port
+	echo  "Running docker on "$port
+	(docker run -p $port:80 gr_nampally_a2 &) || throw 1 # proxy the available port on the parent machine to docker 80 port.
+#Question 7
 
+docker container ps -a | grep gr_nampally | awk '{ print $1 }' > docker_ids #storing the Docker unique Id's in 'docker_ids' file as buffer.
+while read docker_id; do
+	docker_ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $docker_id) #Capturing the respective docker IP address
+	echo "Docker ID: "$docker_id" , IP Address: "$docker_ip
+done < docker_ids
 
+#Question 8
+	sleep 5
+	test_http=$(curl -s -o /dev/null -w "%{http_code}" lnx.cs.smu.ca:$port) #capturing the response header code. if 200 then success, else test case failed.
+	echo $test_http
+	if [ $test_http -eq "200" ]
+	then
+		echo  "Website is working"
+	else
+		echo  "Website cant be reached, terminating the program"
+		throw 1 #throwing an exception as the http didnt return 200 response.
+	fi
 
+#Question 9
+	wget lnx.cs.smu.ca:$port -O serv.html || throw 1
 
+	if [ -f serv.html ]
+		then 
+		echo  "serv.html downloaded"
+	else
+		echo  "serv.html download failed, terminating the program"
+		throw 1 #throwing an exception as the serv.html was not captured.
+	fi
+	echo "Success"
+	destroy_function # cleaning the container and image of docker.
+	exit 0 # Exiting the program with success code.
+	)
+	catch || {
+		#Question 11
+		#checking if all the functions are executed successfully if not garbage collect the local variables.
+		echo "Calling garbage collection"
+		destroy_function
+	}
+}
 
+#catching the inturrupt function and cleaning up the docker image and container
+catch_ctrlc (){
+	echo "Ctrl-C Caught"
+	echo "Calling garbage collection"
+	destroy_function
+	exit 2
+}
 
+#registering a trap to listen to ctrl-C
+trap "catch_ctrlc" 2
 
+#cleaning up the docker container and image
+destroy_function
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#Executing the porgram.
+program
